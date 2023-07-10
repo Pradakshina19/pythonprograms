c340f6e2073360f953cc933771e20181692f9a5a
PRADAKSHINA D <139040212+Pradakshina19@users.noreply.github.com>
2023-07-10 23:25:03 +0530

    Add files via upload

--- sets and its operations.py
+++ sets and its operations.py
@@ -1,7 +1,7 @@
 list1 = [1,2,3,4,5]
 list2 = [1,2,3]
-print("Original list : " + str(list1))
-print("Original sub list : " + str(list2))
+print("Original list : ", list1)
+print("Sub list : ", list2)
 if(all(x in list1 for x in list2)):
     print("Yes, list is subset of other.")
 else:
